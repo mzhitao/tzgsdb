e17a4a9396b8fbacb973586b4859fca83fbe8b2c
mzhitao <maozhitao@sohu.com>
2025-01-15 17:36:45 +0800

    Clear Git cache and re-add files

--- server/dist/routes/project.ts/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-// server/src/routes/userRoutes.ts
-import express from 'express';
-import { createProject, getProjects } from '../../controllers/project/project';
-const router = express.Router();
-// 定义路由
-router.post('/', createProject);
-router.get('/', getProjects);
-export default router;
-//# sourceMappingURL=projectRoutes.js.map
